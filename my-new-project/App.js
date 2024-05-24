c7c5322d5784bcd22eade043c5cbab4191011d79
Gad Mensah <speedsterblack92@gmail.com>
2024-05-24 11:00:53 +0000

    initial commit

---
+++ my-new-project/App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+       My name is <Text style={styles.boldText}>Gad Kwame Mensah</Text>
+        </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text:{
+    fontSize:24,
+  },
+  boldText: {
+    fontWeight: 'bold'
+  }
+});
